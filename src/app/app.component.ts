28c1eed85b31a6a54c98438d1ae346736fc42bf2
Ravi Mohan <ravi.mohan@roonga.com.au>
2024-10-31 06:54:19 +1100

    feat: boostrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonsComponent } from "./buttons/buttons.component";
+import { CheckboxesComponent } from "./checkboxes/checkboxes.component";
+import { MatDividerModule } from '@angular/material/divider';
+import { InputsComponent } from "./inputs/inputs.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatDividerModule, ButtonsComponent, CheckboxesComponent, InputsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/checkboxes/checkboxes.component.ts
@@ -0,0 +1,50 @@
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+export interface Task {
+  name: string;
+  completed: boolean;
+  subtasks?: Task[];
+}
+
+@Component({
+  selector: 'app-checkboxes',
+  standalone: true,
+  templateUrl: './checkboxes.component.html',
+  styleUrl: './checkboxes.component.scss',
+  imports: [MatCheckboxModule, FormsModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CheckboxesComponent {
+  readonly task = signal<Task>({
+    name: 'Parent task',
+    completed: false,
+    subtasks: [
+      { name: 'Child task 1', completed: false },
+      { name: 'Child task 2', completed: false },
+      { name: 'Child task 3', completed: false },
+    ],
+  });
+
+  readonly partiallyComplete = computed(() => {
+    const task = this.task();
+    if (!task.subtasks) {
+      return false;
+    }
+    return task.subtasks.some(t => t.completed) && !task.subtasks.every(t => t.completed);
+  });
+
+  update(completed: boolean, index?: number) {
+    this.task.update(task => {
+      if (index === undefined) {
+        task.completed = completed;
+        task.subtasks?.forEach(t => (t.completed = completed));
+      } else {
+        task.subtasks![index].completed = completed;
+        task.completed = task.subtasks?.every(t => t.completed) ?? true;
+      }
+      return { ...task };
+    });
+  }
+}
